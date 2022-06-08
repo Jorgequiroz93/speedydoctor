9eb81ee47bfa85064a3ae48888640c5749ffe196
manonaidinian <manonaidinian@gmail.com>
2022-06-08 13:42:10 +0800

    basic search bar functionning with specialty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:doctors]
 
   def doctors
-    @doctors = User.all.select { |user| user.role = "doctor" }
+    if params[:query].present?
+      @doctors = User.all.where("specialty ILIKE ?", "%#{params[:query]}%")
+    else
+      @doctors = User.all.select { |user| user.role = "doctor" }
+    end
   end
 end
