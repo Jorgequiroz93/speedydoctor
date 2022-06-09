ad7b31191d6f60d1cb1d3dc2f42a5babd2f45ff7
Yanko Thomson <thomsonyani@gmail.com>
2022-06-09 12:15:20 +0800

    Changed search bar, updating horizontal card, needing to pull new origin master

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/styleguide', to: 'pages#styleguide'
-  
+
   get '/doctors', to: 'users#doctors'
 
-
   get '/dashboard', to: 'pages#dashboard'
- # get '/ourservices' to: 'pages#ourservices'
- # get '/becomespeedy' to: 'pages#becomespeedy'
-  
-  resources :consultations, only: [:show]
 
+  # get '/ourservices' to: 'pages#ourservices'
+  # get '/becomespeedy' to: 'pages#becomespeedy'
+
+  resources :consultations, only: [:show]
 end
