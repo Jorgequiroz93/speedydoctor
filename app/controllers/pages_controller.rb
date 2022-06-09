7798c080fb6c0271f63965fa81f77620fc518441
manonaidinian <manonaidinian@gmail.com>
2022-06-09 16:07:25 +0800

    fixing show access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :becomespeedy ]
+  skip_before_action :authenticate_user!, only: [ :home, :becomespeedy, :show ]
 
   def home
   end
