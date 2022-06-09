298aac8800d1d2d4d5ebc231a945c35bde9fe3f3
Jorge Quiroz <jorge.quiroz.eduardo@gmail.com>
2022-06-09 12:34:32 +0800

    Fixed the routes

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,8 +1,10 @@
 class DoctorsController < ApplicationController
 
     def show
-        @doctor = Doctor.find(params[:id])
-        @doctor_avg_rating = @doctor.reviews.average(:rating)
+        @doctors = User.where(role: "Doctor")
+        @doctor = @doctors.find(params[:id])
+        #@doctor_avg_rating = @doctor.reviews.average(:rating)
         @consultation = Consultation.new()
-      end
+    end
+
 end
