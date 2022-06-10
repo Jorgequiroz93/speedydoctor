1c142bcae7548457a269597c36fa593f49a86afa
Max Matrenitski <matrenitski@gmail.com>
2022-06-10 15:11:55 +0800

    timer saves status with fetch - update consuptation

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
-  resources :doctors, only: [] do 
+  resources :doctors, only: [:show] do
     resources :consultations, only: [:create]
   end
 
-  resources :consultations, only: [:show] do
+  resources :consultations, only: [:show, :update] do
     resources :reviews, only: [:create, :update]
     resources :reports, only: [:create]
   end
 
-  resources :reports, only: [:show]
-  resources :doctors, only: [:show]
-
+  # resources :reports, only: [:show]
+  # resources :doctors, only: [:show]
 
   get '/styleguide', to: 'pages#styleguide'
 
@@ -26,6 +25,4 @@ Rails.application.routes.draw do
 
   # get '/ourservices' to: 'pages#ourservices'
   # get '/becomespeedy' to: 'pages#becomespeedy'
-
-  resources :consultations, only: [:show]
 end
