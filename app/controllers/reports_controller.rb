5e841e4f6c8035d7daca96d2ba873ab043708558
Jorge Quiroz <jorge.quiroz.eduardo@gmail.com>
2022-06-14 10:27:27 +0800

    Reports page advances on the controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,22 @@
 class ReportsController < ApplicationController
+
+    
+    def create
+      @consultation = Consultation.find(params[:consultation_id])
+      @report =  Report.new(report_params)
+      @report.consultation = @consultation
+      if @report.save
+        redirect_to consultation_report_path(@consultation, @report) #Indicate the path and the arguments
+      else
+       render 'consultations/show' #Controller/action
+      end
+
+    end
+
+    private
+
+    def report_params
+      params.require(:report).permit(:content, :prescription)
+    end
+
 end
