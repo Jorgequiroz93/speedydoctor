5e12b11f3234b3a48d26da455837d4c64925f412
Jorge Quiroz <jorge.quiroz.eduardo@gmail.com>
2022-06-15 15:15:34 +0800

    Changes to the doctors profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Consultation.destroy_all
 User.destroy_all
 
 ## special users
-doctor = User.create!(email: "doctor@gmail.com", date_of_birth: 40.years.ago, password:"123123", first_name: "Gregory", last_name: "House", country: "US", language: "English", role: "Doctor", rate: 2.5, specialty: "General", sub_specialty: "Diagnostics", skills: "Piss people off.", prefix: "Dr.", status: "Offline", diplomas:"Ph.D in Critical Care Medicine from Harvard School of Medicine - Boston, MA, USA")
+doctor = User.create!(email: "doctor@gmail.com", date_of_birth: 40.years.ago, password:"123123", first_name: "Gregory", last_name: "House", country: "US", language: "English", role: "Doctor", rate: 2.5, specialty: "General", sub_specialty: "Diagnostics", skills: "Piss people off.", prefix: "Dr.", status: "Offline", diplomas:"Ph.D in Critical Care Medicine from Harvard School of Medicine - Boston, MA, USA" rating: "4.9")
 file = File.open(Rails.root.join("app/assets/images/doctors/doc0.jpg"))
 doctor.photo.attach(io: file, filename: "doc0.jpg", content_type: 'images/doctors/jpg')
 
