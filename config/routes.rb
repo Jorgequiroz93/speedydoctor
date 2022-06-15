c9c65a6264f44c1340c98c1b944f90a4194f0b59
Yanko Thomson <thomsonyani@gmail.com>
2022-06-15 15:19:35 +0800

    final changes for wednesday before presenting to be pushed

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index] do
     member do
-      post 'toggle_favorite', to: "pages#dashboard"
+      post '/dashboard', to: "users#toggle_favorite"
     end
   end
 end
